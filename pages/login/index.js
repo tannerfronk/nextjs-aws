37aa550b07ad0a4e52349702f57bebb7b4ea5ae7
tannerfronk <tannerfronk@gmail.com>
2022-01-26 18:29:14 -0700

    imported login component

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,6 +1,104 @@
-function LoginPage(){
+import { Box, Button, TextField } from '@mui/material'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
+import { useRouter } from 'next/router'
+
+const style = {
+    display: 'flex',
+    justifyContent: 'center',
+    mt: 4
+}
+
+const LoginPage = () => {
+
+    const handleRedirect = () => useRouter.push('/')
+
     return (
-        <h1>this is the login page</h1>
+        <Box sx={style}>
+            <Formik
+                initialValues={{
+                    email: '',
+                    password: ''
+                }}
+                validationSchema={Yup.object().shape({
+                    email: Yup.string()
+                        .email('Must be a valid email')
+                        .max(255)
+                        .required('Email is required'),
+                    password: Yup.string()
+                        .min(8, 'Must be at least 8 characters')
+                        .max(255)
+                        .required('Password is required')
+                })}
+                onSubmit={async (value, { setErrors, setStatus, setSubmitting }) => {
+                    try {
+                        setStatus({ success: true })
+                        setSubmitting(false)
+                        await identity.login({
+                            email: value.email,
+                            password: value.password
+                        }).then(() => {
+                            console.log('Successfully submitted!')
+                            handleRedirect()
+                        })
+                    } catch (e) {
+                        console.log(e)
+                        setStatus({ success: false })
+                        setErrors({ submit: e.message })
+                        setSubmitting(false)
+                    }
+                }}
+            >
+                {({
+                    errors,
+                    values,
+                    handleSubmit,
+                    handleBlur,
+                    handleChange,
+                    isSubmitting,
+                    touched
+                }) => (
+                    <form noValidate onSubmit={handleSubmit}>
+                        <TextField
+                            error={Boolean(touched.email && errors.email)}
+                            fullWidth
+                            helperText={touched.email && errors.email}
+                            label="Email Address"
+                            margin="normal"
+                            name="email"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            type="email"
+                            variant="outlined"
+                            value={values.email}
+                        />
+                        <TextField
+                            error={Boolean(touched.password && errors.password)}
+                            fullWidth
+                            helperText={touched.password && errors.password}
+                            label="Password"
+                            margin="normal"
+                            name="password"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            type="password"
+                            variant="outlined"
+                            value={values.password}
+                        />
+                        <Button
+                            sx={{ mt: 2, backgroundColor: '#F0131E' }}
+                            disabled={isSubmitting}
+                            fullWidth
+                            size="large"
+                            variant="contained"
+                            type="submit"
+                        >
+                            Login
+                        </Button>
+                    </form>
+                )}
+            </Formik>
+        </Box>
     )
 }
 
